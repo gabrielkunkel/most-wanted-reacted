0ddf56f47740de1fca3a7909598c7cf117f5d8b8
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-09-23 07:55:27 -0500

    add mergeSearchObject function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ function App() {
 
   return (
     <div className="App">
+       <form onSubmit={event => {
+        event.preventDefault();
+        filterFromDatabase();
+      }}>
+        <div><input id="firstName" placeholder="First Name" onChange={e => mergeSearchObject({firstName: e.target.value})}/></div>
+        <div><input id="lastName" placeholder="Last Name" onChange={e => mergeSearchObject({lastName: e.target.value})} /></div>
+        <div><input id="eyeColor" placeholder="Eye Color" onChange={e => mergeSearchObject({eyeColor: e.target.value})} /></div>
+        <div><input id="occupation" placeholder="Occupation" onChange={e => mergeSearchObject({occupation: e.target.value})} /></div>
+        <div><input id="height" placeholder="Height" onChange={e => mergeSearchObject({height: parseInt(e.target.value)})} /></div>
+        <div><input id="weight" placeholder="Weight" onChange={e => mergeSearchObject({weight: parseInt(e.target.value)})} /></div>
+        <button>Search</button>
+      </form>
+
+      <br />
+
     </div>
   );
 }
